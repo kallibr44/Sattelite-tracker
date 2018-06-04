0103e4cb651043b094896ec5647567f35b6774ba
kallibr44 <kallibr44@gmail.com>
2018-06-04 22:38:49 +0700

    Update sattrack.py

--- sattrack.py
+++ sattrack.py
@@ -6,12 +6,45 @@ import os
 import time
 import db
 import datetime
+import requests
 
 os.system("clear")
 qth = (56.5, 84.96667, 98)
 tle = """NOAA 19 [+]
 1 33591U 09005A   18011.92186191  .00000120  00000-0  90276-4 0  9994
 2 33591  99.1222 347.0149 0014775  48.2167 312.0264 14.12245172459856"""
+
+def version():
+ version = 1001
+ chk_var = 0
+ try:
+   print("Check internet connection...")
+   reqq = requests.get("https://github.com",timeout=(10))
+ except requests.exceptions.ConnectTimeout:
+    print("No internet connection! skipped...")
+    chk_var = 1
+ if chk_var = 0:  
+  req = requests.get("https://raw.githubusercontent.com/kallibr44/Sattelite-tracker/master/version")
+  raw = req.content
+  print("Your version is: " + str(version))
+  print("Server version is: " + str(raw))
+  if (int(raw) != version):
+   print("New version detected! Loading...")
+   time.sleep(1)
+   print("Downloading new version...")
+   time.sleep(1)
+   os.system("git clone https://github.com/kallibr44/sattelite-tracker")
+   print("Download complete!")
+   time.sleep(1)
+   print("Copying new files...")
+   os.system("sudo cp -rf ./sattelite-tracker ../")
+   print("Copying complete!")
+   time.sleep(1)
+   os.system("sudo rm -r ./sattelite-tracker && rm version")
+   print("All done! Reloading...")
+   os.system("python sattrack.py")
+  else:
+  print("You have latest version! starting...")
 
 def update():
  db.download_update()
@@ -42,6 +75,7 @@ if __name__=='__main__':
   print "  1. Update TLE                                        \n"
   print "  2. Count DB                                          \n"
   print "  3. Start Track                                       \n"
+  print "  4. Check for updates                                 \n"
   print "-------------------------------------------------------\n"
   choose = input("ST@local: ")
   if choose == 1:
